5aa148ab9326c6ab238d7e89bcad51a869f3bdc9
Luis Pérez <luis.perez@geekdacity.com>
2024-01-17 23:10:20 -0600

    Add material & setups

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DogsBookListComponent } from './dogs-book/dogs-book-list/dogs-book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: DogsBookListComponent },
+  { path: 'details/:index', loadComponent: () => import('./dogs-book/dogs-book-detail-view/dogs-book-detail-view.component').then(c => c.DogsBookDetailViewComponent)}
+];
